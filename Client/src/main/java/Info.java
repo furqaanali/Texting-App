50842b0598e994bd8175df27f7f7f83b274998df
Furqaan Ali <fali30@uic.edu>
2020-09-03 22:56:00 -0500

    Handle Networking

---
+++ Client/src/main/java/Info.java
@@ -0,0 +1,6 @@
+import java.io.Serializable;
+
+public class Info implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ Server/src/main/java/GUI.java
@@ -0,0 +1,107 @@
+
+import java.util.HashMap;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class GUI extends Application{
+
+
+	TextField s1,s2,s3,s4, c1;
+	Button serverChoice,b1;
+	HashMap<String, Scene> sceneMap;
+	GridPane grid;
+	HBox buttonBox;
+	Scene startScene;
+	BorderPane startPane;
+	Server serverConnection;
+
+	ListView<String> listItems, listItems2;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("The Networked Client/Server GUI Example");
+
+		this.serverChoice = new Button("Server");
+		this.serverChoice.setStyle("-fx-pref-width: 300px");
+		this.serverChoice.setStyle("-fx-pref-height: 300px");
+
+		this.serverChoice.setOnAction(e->{ primaryStage.setScene(sceneMap.get("server"));
+			primaryStage.setTitle("This is the Server");
+			serverConnection = new Server(data -> {
+				Platform.runLater(()->{
+					listItems.getItems().add(data.toString());
+				});
+
+			});
+
+		});
+
+
+		this.buttonBox = new HBox(400, serverChoice);
+		startPane = new BorderPane();
+		startPane.setPadding(new Insets(70));
+		startPane.setCenter(buttonBox);
+
+		startScene = new Scene(startPane, 800,800);
+
+		listItems = new ListView<String>();
+		listItems2 = new ListView<String>();
+
+		c1 = new TextField();
+		b1 = new Button("Send");
+
+		sceneMap = new HashMap<String, Scene>();
+
+		sceneMap.put("server",  createServerGui());
+
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent t) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+
+
+
+		primaryStage.setScene(startScene);
+		primaryStage.show();
+
+	}
+
+	public Scene createServerGui() {
+
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(70));
+		pane.setStyle("-fx-background-color: coral");
+
+		pane.setCenter(listItems);
+
+		return new Scene(pane, 500, 400);
+
+
+	}
+
+
+}
